51d2dc222fb0b6a9b77b328cd9817600247df84f
wuhu <wuhu@itcast.cn>
2020-03-05 15:09:10 +0800

    箭头函数

--- 02-课堂代码/12-箭头函数返回对象补充.js
+++ 02-课堂代码/12-箭头函数返回对象补充.js
@@ -2,13 +2,13 @@
 
 // 定义一个函数，函数内部返回了一个对象
 const fn1 = function () {
-    return { name: '小明', bb: 11 };
+    return { aa: '小明', bb: 11 };
 };
 const r1 = fn1();
 console.log('function 函数内部返回的对象', r1);
 
-// 补充：箭头函数直接返回一个对象，必须在对象外面加上小括号，否则会报错。
-const fn2 = () => ({ name: '小明2', bb: 22 });
+// 补充了解：箭头函数直接返回一个对象，必须在对象外面加上小括号，否则会报错。
+const fn2 = () => ({ aa: '小明2', bb: 22 });   // 如果是 return 一个对象，对象外要加一个 ()
 const r2 = fn2();
 console.log('箭头函数内部返回的对象', r2);
 
